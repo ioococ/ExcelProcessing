4c6fb744f43aecc4f6fbebf07eb9b0ac2a805558
ioococ <cheerupi@outlook.com>
2024-03-16 14:29:51 +0800

    ✔ add: rabbitmq

---
+++ src/main/java/ink/onei/excel/service/rabbit/Rabbit.java
@@ -0,0 +1,30 @@
+package ink.onei.excel.service.rabbit;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: nekotako
+ * @Description: TODO
+ * @Date: 16/03/2024 10:52 Saturday
+ */
+@Service
+@RabbitListener(queues = "hello")
+public class Rabbit {
+
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    public void send(Object o) {
+        rabbitTemplate.convertAndSend(o);
+    }
+
+    //接受者
+    @RabbitHandler
+    public void process(Object o) {
+        System.out.println("Receiver object : " + o);
+    }
+}
